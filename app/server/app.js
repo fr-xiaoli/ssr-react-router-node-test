2a9bc928e13ce235a1c334e66e682d0735e1f22e
Shen Xiaoli <xiaoli.shen@fastretailing.com>
2018-02-05 12:55:50 +0900

    fix broken server/app.js after testing it

--- app/server/app.js
+++ app/server/app.js
@@ -10,7 +10,7 @@ const app = express();
 // from which all static assets/files will be served.
 const assets = express.static(path.join(__dirname, '../'));
 
-app.use(cors);
+app.use(cors());
 app.use(assets);
 
 app.get('*', router);
